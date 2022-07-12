8f9b5cae5192935ca763b833db6880b26025501f
Jyebin <108725996+Jyebin@users.noreply.github.com>
2022-07-13 02:23:56 +0900

    Add files via upload

---
+++ 혼공씨/p309.c
@@ -0,0 +1,34 @@
+//
+//  p309.c
+//  study
+//
+//  Created by 조예빈 on 2022/07/13.
+//
+
+#include <stdio.h>
+void my_gets(char *str, int size);
+
+int main(void)
+{
+    char str[7]; //문자열을 저장할 배열
+    
+    my_gets(str, sizeof(str)); //한 줄의 문자열을 입력하는 함수
+    printf("입력한 문자열 : %s\n",str); //입력한 문자열 출력
+    
+    return 0;
+}
+
+void my_gets(char *str, int size) //str은 char배열, size는 배열의 크기
+{
+    int ch; //getchar 함수의 반환값을 저장할 변수
+    int i = 0; //str배열의 첨자
+    
+    ch = getchar(); //첫 번째 문자 입력
+    while((ch!='\n')&&(i<size-1)){ //배열의 크기만큼 입력
+        str[i] = ch; //입력한 문자를 배열에 저장
+        i++; //첨자 증가
+        ch = getchar(); //새로운 문자 입력
+    }
+    str[i] = '\0'; //입력한 문자열의 끝에 널 문자를 저장
+    
+}
