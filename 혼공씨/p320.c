6b208c332618d78f8db107d753e1de8ce0e84e83
Jyebin <108725996+Jyebin@users.noreply.github.com>
2022-07-17 16:56:37 +0900

    Add files via upload

---
+++ 혼공씨/p320.c
@@ -0,0 +1,18 @@
+//
+//  p320.c
+//  study
+//
+//  Created by 조예빈 on 2022/07/17.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    char *dessert = "apple"; //포인터에 문자열 초기화
+    
+    printf("오늘 후식은 %s입니다.\n", dessert); //문자열 출력
+    dessert = "banana"; //새로운 문자열 대입
+    printf("내일 후식은 %s입니다.\n",dessert); //바뀐 문자열 출력
+    
+    return 0;
+}
