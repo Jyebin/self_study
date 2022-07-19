29a13a6cb80b7889d77a78768e86945523c1ebd7
Jyebin <yebin0322@naver.com>
2022-07-20 02:19:23 +0900

    2022.07.20

---
+++ 혼공씨/p303.c
@@ -0,0 +1,17 @@
+//
+//  p303.c
+//  study
+//
+//  Created by 조예빈 on 2022/07/20.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    char ch;
+    
+    printf("문자 입력 : "); //문자 입력
+    scanf("%c",&ch);
+    printf("%d",ch);
+    return 0;
+}
