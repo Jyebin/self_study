16b3d532819a2095feb108a6c73b3a0a85e4f0f8
Jyebin <yebin0322@naver.com>
2022-07-24 15:38:24 +0900

    2022.07.24

---
+++ 혼공씨/p340.c
@@ -0,0 +1,20 @@
+//
+//  p340.c
+//  study
+//
+//  Created by 조예빈 on 2022/07/24.
+//
+
+#include <stdio.h>
+#include <string.h> //strncpy 함수 사용을 위한 헤더 파일 포함
+
+int main(void)
+{
+    char str[20] = "mango tree"; //배열 초기화
+    
+    strncpy(str, "apple-pie", 5); //"apple-pie"에서 다섯 문자만 복사
+    
+    printf("%s\n", str); //복사 받은 문자열 출력
+    
+    return 0;
+}
